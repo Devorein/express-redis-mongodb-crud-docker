6d733b0ee694edd26217c6797f474882ee76e8ac
Safwan Shaheer <devorein00@gmail.com>
2021-05-07 20:11:38 +0600

    Init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
+
+app.get("/",(_,res)=>{
+  res.send("<h2>Hello World</h2>")
+})
