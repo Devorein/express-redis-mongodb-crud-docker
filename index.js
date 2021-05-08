10ada86dc075adb047393b8af327e06aed902081
Safwan Shaheer <devorein00@gmail.com>
2021-05-08 20:56:44 +0600

    Added scripts for nodemon

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
 
 app.get("/",(_,res)=>{
-  res.send("<h2>Hello World !</h2>")
+  res.send("<h2>Hello World</h2>")
 })
