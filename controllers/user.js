9f5ea87bce1be2fd8d32951d901b0345cc971648
Safwan Shaheer <devorein00@gmail.com>
2021-05-09 18:05:58 +0600

    Implemented argon2 password hashing

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,24 @@
+const argon2 = require('argon2');
 const User = require('../models/User');
 
 async function signup(req, res, next) {
   try {
-    const user = await User.create(req.body);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        user
-      }
-    });
+    try {
+      const hashedPassword = await argon2.hash(req.body.password);
+      req.body.password = hashedPassword;
+      const user = await User.create(req.body);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          user
+        }
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 'failure',
+        message: err.message
+      });
+    }
   } catch (err) {
     res.status(400).json({
       status: 'failure',
