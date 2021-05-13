0dbe00e60512d8bc392dd03b02d19b3693638dcb
Safwan Shaheer <devorein00@gmail.com>
2021-05-14 00:00:16 +0600

    Added updateUser route and controller

--- controllers/user.js
+++ controllers/user.js
@@ -34,7 +34,28 @@ const getUserById = async (req, res, next) => {
   }
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const { user } = req.session;
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: await User.findByIdAndUpdate(user._id, req.body, {
+          new: true,
+          runValidators: true
+        })
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failure',
+      message: err.message
+    });
+  }
+};
+
 module.exports = {
   getAllUsers,
-  getUserById
+  getUserById,
+  updateUser
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const UserController = require('../controllers/user');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/').get(UserController.getAllUsers);
+router
+  .route('/')
+  .get(UserController.getAllUsers)
+  .patch(authMiddleware, UserController.updateUser);
 
 router.route('/:id').get(UserController.getUserById);
 
